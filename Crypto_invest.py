6be6f827eee8fde5d7f6c904f98e059f37b40bf9
MiraKanec <MiraKanec@users.noreply.github.com>
2017-06-06 19:57:34 +1200

    second version

--- Crypto_invest.py
+++ Crypto_invest.py
@@ -1,54 +1,87 @@
 
-from numpy import genfromtxt
 import os
+from numpy import genfromtxt
+from coinmarketcap_library import Market
+from coinmarketcap_library import CoinMarket	
+from time import sleep
+
+
 #clean screen
 os.system('cls')
+print ('\n')
 
-# array of currency price
-CURR_coindesk = genfromtxt('C:\Prog\Crypto_invest_my_proj\\training_CSV\ETH1.csv', delimiter=',')
-
-
-##############################
-
-money_I_have =   0.0
-tmp_price    =  161.18
-tmp_volume   =  100.0/tmp_price
-
-##############################
 
 # bool - used as decision if next action is BUY or SELL 
 buy = False
-# 
-new_max_price = tmp_price
 
 
-for actual_price in CURR_coindesk:
+##############################
+#definuj pocatecni hodnoty
+money_i_have = 0.0
+tmp_price    = 0.00598069 #price in USD
+tmp_volume   = 100.0/tmp_price
+timestamp_now = 1496582052
+timespamp_before = 1496582052
+
+# price of single stock I bought last time
+stock_bought_for = tmp_price
+
+print("Start: \nstart price:", tmp_price, "start volume:", tmp_volume)
+print("  Seeking for new price:", stock_bought_for*1.05, "Actual price is:", tmp_price)
+##############################
+
+
+# create new class instance 
+bitcoin = CoinMarket()
+
+while True:
+
+	# update data for 
+	bitcoin_usd_price, timestamp_now = bitcoin.get_data("digitalnote")
+	# print(bitcoin_usd_price)
+	# print(digitalnote_last_updated)
+
+
+
 	# search for BUY
 	if buy:
-		print("new_max_price =", new_max_price, "new_max_price*0.95 =", new_max_price*0.95, "actual_price =", actual_price)
+		
 		# if price dropped 5% from last sale -> buy again
-		if (new_max_price*0.95) >= actual_price:
-			tmp_volume = money_I_have/actual_price
-			money_I_have = 0
-			tmp_price = actual_price # save price 
-			print("BOUGHT! tmp_volume =",tmp_volume, "PAID each:", actual_price)
-			print("  sell when price =", actual_price*1.05)
+		if (stock_bought_for*0.95) >= bitcoin_usd_price:
+			tmp_volume = money_i_have/bitcoin_usd_price
+			money_i_have = 0
+			tmp_price = bitcoin_usd_price # save price 
+			print("BOUGHT! Volume:",tmp_volume, "PAID for each:", bitcoin_usd_price, 
+				  "  sell when price >=", bitcoin_usd_price*1.05)
 			buy = False
 		else:
-			print("no buy")
+			#say "no buy"
+			if timestamp_now != timespamp_before:
+				print("  no buy. Last stock bought for:", stock_bought_for, "Seeking for new price:", stock_bought_for*0.95, "Actual price is:", bitcoin_usd_price)
+				timespamp_before = timestamp_now
 			
 	# search for SELL
 	else:		
-		if actual_price >= (tmp_price*1.05):
+		if bitcoin_usd_price >= (tmp_price*1.05):
 			#sell
-			money_I_have = actual_price * tmp_volume
-			print("SOLD!  money_I_have =", money_I_have, "SOLD each:", actual_price)
+			money_i_have = bitcoin_usd_price * tmp_volume
+			print("SOLD!  Money I have:", money_i_have, "SOLD each for:", bitcoin_usd_price)
 			
 			buy = True
-			new_max_price = actual_price
+			#NEFUNGUJE timespamp_before = 0 # vynuluj citac a vypis "no buy" at vis jakou cenu hledas
+			stock_bought_for = bitcoin_usd_price
 			
 		else:
 			#say "no sale"
-			print("no sale  ", actual_price)
+			if timestamp_now != timespamp_before:
+				print("  no sale. Seeking for new price:", stock_bought_for*1.05, "Actual price is:", bitcoin_usd_price)
+				timespamp_before = timestamp_now
 
-print("money_I_have =", money_I_have)
+	# 
+	sleep(60)	
+	
+	
+	
+	
+	
+	
